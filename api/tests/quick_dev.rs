20a341f58f4ac5b8b808edf9126c8ea49ce59e6d
rjlacanlaled <rjlacanlaled@gmail.com>
2023-10-20 20:26:59 +0800

    add initial client tests

---
+++ api/tests/quick_dev.rs
@@ -0,0 +1,10 @@
+use anyhow::{Ok, Result};
+use serde_json::json;
+use tokio::test;
+
+#[test]
+async fn quick_dev() -> Result<()> {
+    //    let hc = httpc_test::new_client("http://localhost:8080")?;
+
+    Ok(())
+}
